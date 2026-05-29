1c8bd050b95d86da42169df6c4534815213dbc05
GitHubUserSeshat <51201787+GitHubUserSeshat@users.noreply.github.com>
2026-05-29 10:30:38 +0200

    Add files via upload

---
+++ data/levels.js
@@ -0,0 +1,78 @@
+const LEVELS = [
+    {
+        name: "Level 1",
+        speed: 0.8,
+        rounds: 5
+    },
+    {
+        name: "Level 2",
+        speed: 1.2,
+        rounds: 5
+    },
+    {
+        name: "Level 3",
+        speed: 1.8,
+        rounds: 5
+    }
+];
+
+// =====================================================
+// SYMBOL DATABASE (IMPORTANT)
+// key = AUDIO FILE NAME
+// text = DISPLAY TEXT
+// =====================================================
+
+const SYMBOLS = [
+    { key: "a", text: "a" },
+    { key: "b", text: "b" },
+    { key: "c", text: "c" },
+    { key: "d", text: "d" },
+    { key: "e", text: "e" },
+    { key: "f", text: "f" },
+    { key: "g", text: "g" },
+    { key: "h", text: "h" },
+    { key: "i", text: "i" },
+    { key: "j", text: "j" },
+    { key: "k", text: "k" },
+    { key: "l", text: "l" },
+    { key: "m", text: "m" },
+    { key: "n", text: "n" },
+    { key: "o", text: "o" },
+    { key: "p", text: "p" },
+    { key: "q", text: "q" },
+    { key: "r", text: "r" },
+    { key: "s", text: "s" },
+    { key: "t", text: "t" },
+    { key: "u", text: "u" },
+    { key: "v", text: "v" },
+    { key: "w", text: "w" },
+    { key: "x", text: "x" },
+    { key: "y", text: "y" },
+    { key: "z", text: "z" },
+
+    { key: "ae", text: "ä" },
+    { key: "oe", text: "ö" },
+    { key: "ue", text: "ü" },
+
+    { key: "au", text: "au" },
+    { key: "da", text: "da" },
+    { key: "david", text: "David" },
+    { key: "ein", text: "ein" },
+    { key: "es", text: "es" },
+    { key: "essen", text: "essen" },
+    { key: "eu", text: "eu" },
+    { key: "frueh", text: "früh" },
+    { key: "heu", text: "Heu" },
+    { key: "luisa", text: "Luisa" },
+    { key: "ma", text: "ma" },
+    { key: "mama", text: "Mama" },
+    { key: "packen", text: "packen" },
+    { key: "papa", text: "Papa" },
+    { key: "schoe", text: "schö" },
+    { key: "schoen", text: "schön" },
+    { key: "schoenen", text: "schönen" },
+    { key: "steh", text: "steh" },
+    { key: "stueck", text: "Stück" },
+    { key: "zug", text: "Zug" },
+    { key: "ben", text: "Ben" }
+];
